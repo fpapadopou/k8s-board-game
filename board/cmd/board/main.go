121ce2bf11456c0a761c771450883a59df11ebf2
Fotis Papadopoulos <fpapadopou@gmail.com>
2020-06-20 14:51:13 +0200

    Add services code
    This introduces two dummy HTTP services. One of them
    represents a board game where the client can attempt to
    guess one the numbers on the board. The other is just a client
    that can "play" on multiple boards at the same time.

---
+++ board/cmd/board/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const (
+	odd  = "odd"
+	even = "even"
+)
+
+var numbers map[int]int
+var score map[string]int
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	score["total"]++
+	param, ok := r.URL.Query()["number"]
+	number, err := strconv.Atoi(param[0])
+
+	if !ok || err != nil {
+		_, _ = w.Write([]byte("required query parameter `number` is missing"))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if _, ok := numbers[number]; !ok {
+		score["fail"]++
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	score["success"]++
+	_, _ = w.Write([]byte("gotcha!"))
+	return
+}
+
+func scoreHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte(fmt.Sprintf("score: %+v", score)))
+	return
+}
+
+func main() {
+
+	log.Println("booting up number guess board game app..")
+
+	oddOrEven := os.Getenv("odd_or_even")
+	if oddOrEven == "" {
+		oddOrEven = odd
+	}
+
+	remainder := 0
+	if oddOrEven == even {
+		remainder = 1
+	}
+
+	score = make(map[string]int)
+	score["total"] = 0
+	score["success"] = 0
+	score["fail"] = 0
+
+	numbers = make(map[int]int)
+	for i := 0; i < 20; i++ {
+		if i%2 == remainder {
+			numbers[i] = i
+		}
+	}
+
+	log.Printf("numbers are %v", numbers)
+
+	http.HandleFunc("/guess", handler)
+	http.HandleFunc("/score", scoreHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ client/cmd/client/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	log.Println("booting up number guess board game client..")
+
+	boardUrls := os.Getenv("boards")
+	if boardUrls == "" {
+		log.Print("no boards provided, shutting down..")
+	}
+
+	boards := strings.Split(boardUrls, ",")
+
+	var wg sync.WaitGroup
+	// Start making calls to the boards' endpoints
+	for _, board := range boards {
+		wg.Add(1)
+		go func(group *sync.WaitGroup, board string) {
+			playBoard(board)
+
+			wg.Done()
+		}(&wg, board)
+	}
+
+	log.Printf("playing on %d boards", len(boards))
+	wg.Wait()
+
+	log.Printf("done playing, shutting down")
+}
+
+func playBoard(boardUrl string) {
+
+	rand.Seed(time.Now().UnixNano())
+
+	tts := 2 * time.Second
+	for {
+
+		time.Sleep(tts)
+
+		number := strconv.Itoa(rand.Intn(20))
+
+		// Give up after some retries.
+		if tts > 31*time.Second {
+			log.Printf("board at %s is not available, giving up", boardUrl)
+			return
+		}
+
+		resp, err := http.Get(boardUrl + "/guess?number=" + number)
+		if err != nil {
+			tts = tts * 2
+			log.Printf("failed to make request to board %s: %v \nretrying in %v", boardUrl, err, tts)
+			continue
+		}
+
+		tts = 2 * time.Second
+		log.Printf("tried number: %s", number)
+		log.Printf("got [%s] response from board", resp.Status)
+	}
+}
